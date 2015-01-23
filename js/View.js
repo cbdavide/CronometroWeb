972febd16de440560fe8df02fa0d0b0143edce3f
David Castelblanco Benavides <david.castelblanco@hotmail.com>
2015-01-23 17:17:31 -0500

    Se terminó el objeto vista

--- js/View.js
+++ js/View.js
@@ -1,24 +1,49 @@
-function View(i){
+function View(i,crono){
   var index = i;
+  var cronometro = crono;
   var template = document.getElementById('cronometro-template').innerHTML;
   var elementos = {};
   this.view = {
+    'id_caja' : 'cronometro'+index,
     'horas': '00',
     'minutos': '00',
     'segundos': '00',
     'id_horas': 'ch'+index,
     'id_minutos': 'cm'+index,
     'id_segundos': 'cs'+index,
+    'id_btn_retroceder': 'cbr'+index,
+    'id_btn_pause': 'cbp'+index,
+    'id_btn_avanzar': 'cba'+index
   };
   this.rendered = Mustache.render(template,this.view);
-  this.add = function(){
+  this.init = function(){
+    this.print();
+    this.initElementos();
+    this.addListener();
+  };
+  this.print = function(){
     //Append de the element in the html
-    document.getElementById('contenido').innerHTML = this.rendered;
+    var section = document.getElementById('contenido');
+    var temp_section = section.innerHTML;
+    section.innerHTML = temp_section + this.rendered;
   };
   this.initElementos = function(){
-    elementos.labelHoras = document.getElementById('ch'+index);
-    elementos.labelMinutos = document.getElementById('cm'+index);
-    elementos.labelSegundos = document.getElementById('cs'+index);
+    //Labels
+    elementos.labelHoras = document.getElementById(this.view.id_horas);
+    elementos.labelMinutos = document.getElementById(this.view.id_minutos);
+    elementos.labelSegundos = document.getElementById(this.view.id_segundos);
+    //Botones
+    elementos.btnRetroceder = document.getElementById(this.view.id_btn_retroceder);
+    elementos.btnPausa = document.getElementById(this.view.id_btn_pause);
+    elementos.btnAvanzar = document.getElementById(this.view.id_btn_avanzar);
+  };
+  /**
+   * Añadir el listener de los botones
+   */
+  this.addListener = function(){
+    // elementos.btnPausa.addEventListener('click',pausar);
+    elementos.btnAvanzar.addEventListener('click',avanzar);
+    elementos.btnRetroceder.addEventListener('click',retroceder);
   };
 
   this.update = function(horas,minutos,segundos){
@@ -33,4 +58,47 @@ function View(i){
     elementos.labelMinutos.innerHTML = minutos;
     elementos.labelSegundos.innerHTML = segundos;
   };
+
+  function pausar(){
+    //Habilitar y deshabilitar botones
+    elementos.btnPausa.removeEventListener('click',pausar);
+    elementos.btnRetroceder.addEventListener('click',retroceder);
+    elementos.btnAvanzar.addEventListener('click',avanzar);
+    //Añadir o quitar clase disabled
+    elementos.btnPausa.classList.add('disabled');
+    elementos.btnAvanzar.classList.remove('disabled');
+    elementos.btnRetroceder.classList.remove('disabled');
+    //Pausar
+    cronometro.setEstado('pause');
+    console.log('pausado');
+  }
+
+  function avanzar(){
+    //Habilitar y deshabilitar botones
+    elementos.btnPausa.addEventListener('click',pausar);
+    elementos.btnAvanzar.removeEventListener('click',avanzar);
+    elementos.btnRetroceder.addEventListener('click',retroceder);
+    //Añadir o quitar clase disabled
+    elementos.btnPausa.classList.remove('disabled');
+    elementos.btnAvanzar.classList.add('disabled');
+    elementos.btnRetroceder.classList.remove('disabled');
+    //Avanzar
+    cronometro.setEstado('avanzar');
+    console.log('avanzar');
+  }
+
+  function retroceder(){
+    //Habilitar y deshabilitar botones
+    elementos.btnPausa.addEventListener('click',pausar);
+    elementos.btnAvanzar.addEventListener('click',avanzar);
+    elementos.btnRetroceder.removeEventListener('click',retroceder);
+    //Añadir o quitar clase disabled
+    elementos.btnPausa.classList.remove('disabled');
+    elementos.btnAvanzar.classList.remove('disabled');
+    elementos.btnRetroceder.classList.add('disabled');
+    //Retroceder
+    cronometro.setEstado('retroceder');
+    console.log('retroceder')
+  }
+
 }
--- js/main.js
+++ js/main.js
@@ -1,29 +1,5 @@
 var cronos = new Cronometro();
-var view = new View(1);
-view.add();
-view.initElementos();
+var view = new View(1,cronos);
+view.init();
 cronos.setObservador(view);
 cronos.loop();
-// Botones
-  var pause = document.getElementById('pause');
-  var forward = document.getElementById('avanzar');
-  var backward = document.getElementById('retroceder');
-//Eventos
-  pause.addEventListener('click',pausar);
-  forward.addEventListener('click',avanzar);
-  backward.addEventListener('click',retroceder);
-//Funciones
-  function pausar(){
-    cronos.setEstado('pause');
-    console.log('pausado');
-  };
-
-  function avanzar(){
-    cronos.setEstado('avanzar');
-    console.log('Avanzando');
-  };
-
-  function retroceder(){
-    cronos.setEstado('retroceder');
-    console.log('Retroceder');
-  };
