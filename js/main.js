30a617b9e2a7f985460de1c6a961c02f8f24e6fc
David Castelblanco Benavides <david.castelblanco@hotmail.com>
2015-01-23 19:25:54 -0500

    Versión 0.9, Aplicación terminada

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-var cronos = new Cronometro();
-var view = new View(1,cronos);
-view.init();
-cronos.setObservador(view);
-cronos.loop();
+var app = {
+  'add': document.getElementById('add'),
+  'contador': 0,
+  'cronometros': []
+};
+app.add.addEventListener('click',function(){
+  var temp = new MakeCronometro(app.contador);
+  temp.init();
+  app.cronometros.push(temp);
+  app.contador++;
+});
+function MakeCronometro(i){
+  this.cronometro = new Cronometro();
+  this.view = new View(i,this.cronometro);
+  this.init = function(){
+    this.view.init();
+    this.cronometro.setObservador(this.view);
+    this.cronometro.loop();
+  };
+}
